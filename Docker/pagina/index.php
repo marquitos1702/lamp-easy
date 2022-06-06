d75eb7731ad35d73c842f42b4017b5fa373d5b5b
marquitos1702 <106312022+marquitos1702@users.noreply.github.com>
2022-06-06 21:29:55 +0200

    Add files via upload

---
+++ Docker/pagina/index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+                echo "<h1>Bienvenidos a prueba empresa</h1>";
+
+                $conn = mysqli_connect('db', 'root', 'test', "basededatos");
+
+                $query = 'SELECT * From Person';
+                $result = mysqli_query($conn, $query);
+
+                echo '<table class="table table-striped">';
+                echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+                while($value = $result->fetch_array(MYSQLI_ASSOC)){
+                    echo '<tr>';
+                    echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+                    foreach($value as $element){
+                        echo '<td>' . $element . '</td>';
+                    }
+
+                    echo '</tr>';
+                }
+                echo '</table>';
+
+                $result->close();
+                mysqli_close($conn);
+            ?>
+        </div>
+    </body>
+</html>
