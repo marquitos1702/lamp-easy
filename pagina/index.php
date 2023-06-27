0358e993d4131263fca054c9c01f88999a0213a8
Marc <106312022+marquitos1702@users.noreply.github.com>
2023-06-28 01:28:51 +0200

    Update index.php

--- pagina/index.php
+++ pagina/index.php
@@ -1,13 +1,60 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Welcome to LAMP Infrastructure</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <div class="container-fluid">
-            <?php
-                echo "<h1>Hola mundo</h1>";
-            ?>
-        </div>
-    </body>
+<head>
+  <title>Horario</title>
+  <style>
+    table {
+      width: 100%;
+      border-collapse: collapse;
+    }
+    
+    th, td {
+      border: 1px solid black;
+      padding: 8px;
+      text-align: left;
+    }
+    
+    th {
+      background-color: #f2f2f2;
+    }
+  </style>
+</head>
+<body>
+  <h1>Horario Semanal</h1>
+  
+  <table>
+    <tr>
+      <th>Hora</th>
+      <th>Lunes</th>
+      <th>Martes</th>
+      <th>Miércoles</th>
+      <th>Jueves</th>
+      <th>Viernes</th>
+    </tr>
+    <tr>
+      <td>9:00 - 10:00</td>
+      <td>Reunion 1</td>
+      <td></td>
+      <td>Reunion 2</td>
+      <td>Reunion 3</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>10:00 - 11:00</td>
+      <td></td>
+      <td>Reunion 4</td>
+      <td></td>
+      <td>Reunion 5</td>
+      <td>Reunion 6</td>
+    </tr>
+    <tr>
+      <td>11:00 - 12:00</td>
+      <td>Reunion 7</td>
+      <td></td>
+      <td>Reunion 8</td>
+      <td></td>
+      <td>Reunion 9</td>
+    </tr>
+  </table>
+</body>
 </html>
